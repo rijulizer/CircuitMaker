055472dc6bb13a49ab61ae2dfb60c953e3233fd0
Psyqe <88829544+psyqe@users.noreply.github.com>
2022-08-22 18:41:54 +0530

    Update universal_gates.py
    Feature addition - dynamic input in universal gates

--- src/digital_circuits/universal_gates.py
+++ src/digital_circuits/universal_gates.py
@@ -1,1 +1,36 @@
+from math import prod
 
+class UniversalGates:
+
+    def __init__(self):
+        pass
+
+    def U_NAND(self, *inp):
+
+        # Check if no arguments were provided
+        if not inp:
+            raise KeyError("input to NAND gate can not be Empty")
+        
+        # Check if non-binary inputs were provided
+        else:
+            for i in inp:
+                if i not in ([0,1]):
+                    raise KeyError("input to OR gate should be 0 or 1")
+        
+        # Perform logical NAND operation
+        return 1-prod(inp)
+
+    def U_NOR(self, *inp):
+
+        # Check if no arguments were provided
+        if not inp:
+            raise KeyError("input to NOR gate can not be Empty")
+        
+        # Check if non-binary inputs were provided
+        else:
+            for i in inp:
+                if i not in ([0,1]):
+                    raise KeyError("input to NOR gate should be 0 or 1")
+        
+        # Perform logical NOR operation
+        return (0 if sum(inp) > 0 else 1)
