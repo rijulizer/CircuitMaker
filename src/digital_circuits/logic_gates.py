801d6613c8de63ed514b54ee25b8f4be601adb69
Psyqe <88829544+psyqe@users.noreply.github.com>
2022-08-22 19:21:52 +0530

    Update logic_gates.py
    Feature addition - dynamic input

--- src/digital_circuits/logic_gates.py
+++ src/digital_circuits/logic_gates.py
@@ -1,65 +1,67 @@
+from math import prod
+
+
 class LogicGates:
     def __init__(self):
         pass
     
-    def NOT(self, inp= None):
+    def NOT(self, *inp):
         """
         Logically implements not gate without using the python not()
         """
-        if inp in ([None]):
-            raise KeyError("input to NOT gate can not be None")
-        if inp == 0:
-            return 1
-        elif inp ==1:
-            return 0
-        else:
+        # Check if no arguments were provided
+        if not inp:
+            raise KeyError("input to NOT gate can not be Empty")
+
+        # Check if multiple inputs were provided
+        if len(inp) > 1:
+            raise KeyError("NOT gate only accepts a single input")
+        
+        # Check if non-binary inputs were provided
+        if inp[0] not in ([0,1]):
             raise KeyError("input to NOT gate should be 0 or 1")
+
+        # Perform logical NOT operation
+        return 1-inp[0]
+
+        # Using universal gates
+        # return self.ug.U_NAND(inp[0])
     
-    def AND(self, inp_a=None, inp_b=None):
+    def AND(self, *inp):
         """
         Logically implements and gate, without using the python and(), and by mimicking how AND works 
         """
-        if None in ([inp_a, inp_b]):
-            raise KeyError("input to AND gate can not be None")
-        if inp_a == 0:
-            if inp_b == 0:
-                return 0
-            if inp_b == 1:
-                return 0
-            else:
+        if not inp:
+            raise KeyError("input to AND gate can not be Empty")
+        
+        # Check if non-binary inputs were provided
+        for i in inp:
+            if i not in ([0,1]):
                 raise KeyError("input to AND gate should be 0 or 1")
-        elif inp_a == 1:
-            if inp_b == 0:
-                return 0
-            if inp_b == 1:
-                return 1
-            else:
-                raise KeyError("input to AND gate should be 0 or 1")
-        else:
-            raise KeyError("input to AND gate should be 0 or 1")
 
-    def OR(self, inp_a=None, inp_b=None):
+        # Perform logical AND operation
+        return prod(inp)
+
+        # Using universal gates
+        # return self.ug.U_NAND(self.ug.U_NAND(inp))
+
+    def OR(self, *inp):
         """
         Logically implements or gate, without using the python or(), and by mimicking how OR works 
         """
-        if None in ([inp_a, inp_b]):
-            raise KeyError("input to OR gate can not be None")
-        if inp_a == 0:
-            if inp_b == 0:
-                return 0
-            if inp_b == 1:
-                return 1
-            else:
+        if not inp:
+            raise KeyError("input to OR gate can not be Empty")
+        
+        # Check if non-binary inputs were provided
+        for i in inp:
+            if i not in ([0,1]):
                 raise KeyError("input to OR gate should be 0 or 1")
-        elif inp_a == 1:
-            if inp_b == 0:
-                return 1
-            if inp_b == 1:
-                return 1
-            else:
-                raise KeyError("input to OR gate should be 0 or 1")
-        else:
-            raise KeyError("input to OR gate should be 0 or 1")
+
+        # Perform logical OR operation
+        return (1 if sum(inp) > 0 else 0)
+
+        # Using universal gates
+        # return self.ug.U_NOR(self.ug.U_NOR(inp))
 
     def NAND(self, inp_a=None, inp_b=None):
         """
@@ -84,25 +86,17 @@ class LogicGates:
 
         return self.NOT(self.OR(inp_a, inp_b))
     
-    def XOR(self, inp_a=None, inp_b=None):
+    def XOR(self, *inp):
         """
         Logically implements xor gate, by mimicking how XOR works 
         """
-        if None in ([inp_a, inp_b]):
-            raise KeyError("input to XOR gate can not be None")
-        if inp_a == 0:
-            if inp_b == 0:
-                return 0
-            if inp_b == 1:
-                return 1
-            else:
+        if not inp:
+            raise KeyError("input to XOR gate can not be Empty")
+        
+        # Check if non-binary inputs were provided
+        for i in inp:
+            if i not in ([0,1]):
                 raise KeyError("input to XOR gate should be 0 or 1")
-        elif inp_a == 1:
-            if inp_b == 0:
-                return 1
-            if inp_b == 1:
-                return 0
-            else:
-                raise KeyError("input to XOR gate should be 0 or 1")
-        else:
-            raise KeyError("input to XOR gate should be 0 or 1")
+
+        # Perform logical XOR operation
+        return sum(inp)%2
